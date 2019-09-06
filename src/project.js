ef2f2b5d6f52014fc3a4afa7bdd04407d44122dc
Joel Stockamp <stockampj@gmail.com>
2019-09-06 14:36:18 -0700

    add a solar sytem class to hold each planet in an array

--- src/project.js
+++ src/project.js
@@ -19,9 +19,10 @@ export class Person {
 }
 
 export class Planet {
-  constructor(name, solarConverter) {
+  constructor(name, solarConverter, position) {
     this.name = name;
     this.solarConverter = solarConverter
+    this.position = position
   }
   calcUserAge(user){
     var convertedAge = ((user.age)/(this.solarConverter)).toFixed(1);
@@ -30,10 +31,32 @@ export class Planet {
   }
 }
 
+class SolarSystem {
+  constructor() {
+    var mercury = new Planet("Mercury", .24, 1);
+    var venus = new Planet("Venus", .62, 2);
+    var earth = new Planet("Earth", 1, 3);
+    var mars = new Planet("Mars", 1.88, 4);
+    var jupiter = new Planet ("Jupiter", 11.86, 5);
+    var saturn = new Planet ("Saturn", 29.48, 6);
+    var uranus = new Planet ("Uranus", 84.07, 7);
+    var neptune = new Planet ("Neptune", 164.8, 8);
+    var pluto = new Planet ("Pluto", 164.8, 9);
+    this.system = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto];
+  }
+
+  getPlanet(name) {
+    var system = this.system;
+    var reference = name;
+    for (var i=0;i<9;i++) {
+      if (system[i].name = reference) {
+        return system[i]
+      }
+    }
+  }
+  
+}
+
+
 var joel = new Person ("Joel", 36, "male");
 joel.calcExp();
-
-var mercury = new Planet("Mercury", .24);
-var venus = new Planet("Venus", .62);
-var mars = new Planet("Mars", 1.88);
-var jupiter = new Planet ("Jupiter", 11.86);
