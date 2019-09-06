285a7aec92a1ab7f5935b46cb93002b534305ac0
Joel Stockamp <stockampj@gmail.com>
2019-09-06 10:43:05 -0700

    add first spec and past object age holder test

--- spec/player-spec.js
+++ spec/player-spec.js
@@ -1,1 +1,11 @@
-import { Game} from './../src/project.js';
+import { Person } from './../src/project.js';
+
+
+describe ('Person', function(){
+  it('checks if Person objects age is being held by object', function(){
+    var joel = new Person ("Joel", 36, "male");
+    expect(joel.age).toEqual(36)
+    expect(joel.name).toEqual("Joel")
+    expect(joel.sex).toEqual("male")
+  });
+});
---
+++ src/project.js
@@ -0,0 +1,8 @@
+export class Person {
+  constructor(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex
+    this.lifeExpectancy= "";
+  }
+}
