b85b069b3362063aa88da31bea2acaa80f660ab9
Joel Stockamp <stockampj@gmail.com>
2019-09-06 09:16:09 -0700

    add test environment

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import {Round, Character, Loot} from './project';
+
+$(document).ready(function(){
+
+});
+// var myGame = new Game
