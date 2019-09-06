33195779f92a4cd07b62289bd0123c9b3d962a24
Joel Stockamp <stockampj@gmail.com>
2019-09-06 12:28:32 -0700

    fix a weird rounding error causing a failure for spec 2

--- spec/solarage-spec.js
+++ spec/solarage-spec.js
@@ -9,3 +9,21 @@ describe ('Person', function(){
     expect(joel.sex).toEqual("male")
   });
 });
+
+describe ('Person', function(){
+  it('checks if life expectancy method calculates remaining years correctly', function(){
+    var joel = new Person ("Joel", 36, "male");
+    var jill = new Person ("Jill", 36, "female");
+    var frank = new Person ("Frank", 36, "");
+    joel.calcExp();
+    console.log(joel.name, joel.age, joel.lifeRemaining)
+    jill.calcExp();
+    console.log(jill.name, jill.age, jill.lifeRemaining)
+    frank.calcExp();
+    console.log(frank.name, frank.age, frank.lifeRemaining)
+    expect(joel.lifeRemaining).toEqual(40.9);
+    expect(jill.lifeRemaining).toEqual(45.6);
+    expect(frank.lifeRemaining).toEqual(43.3);
+
+  });
+});
--- src/project.js
+++ src/project.js
@@ -2,7 +2,30 @@ export class Person {
   constructor(name, age, sex) {
     this.name = name;
     this.age = age;
-    this.sex = sex
-    this.lifeExpectancy= "";
+    this.sex = sex;
+    this.lifeRemaining;
   }
+
+  calcExp() {
+    if (this.sex === "male") {
+      this.lifeRemaining = parseFloat(76.9 - this.age).toFixed(1);
+    } else if (this.sex === "female") {
+      this.lifeRemaining = parseFloat(81.6 - this.age).toFixed(1);
+    } else {
+      this.lifeRemaining = parseFloat(79.3 - this.age).toFixed(1);
+    }
+    this.lifeRemaining = parseFloat(this.lifeRemaining);
+  };
 }
+
+
+
+
+
+
+
+// class Planet {
+//   constructor(name, solarConverter) {
+//     this.name = name;
+//   }
+// }
